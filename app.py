8c10701ffd044cf25b9880c8ea1980e9073309d1
mukagen <tech0.mukainakano@gmail.com>
2025-02-09 14:45:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from db_control import models, schemas, crud, database
